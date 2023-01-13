b7592540765344304d03d54c8befc2773d65a409
Peter Eijgermans <p.eijgermans11@gmail.com>
2023-01-13 15:42:34 +0100

    outdoor events nepal

--- micro-frontend-example/pages/src/OutdoorEventsNepal.tsx
+++ micro-frontend-example/pages/src/OutdoorEventsNepal.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {restaurants} from "./data";
+import {outdoorEventsNepal} from "./data";
 
 export default function Restaurants() {
-  console.log(restaurants);
+  console.log(outdoorEventsNepal);
   return (
     <div className="my-4 px-4 text-center">
       <h1 className="text-gray-600 text-xl mb-2">Recipes</h1>
       <div className="grid grid-cols-3 gap-4">
-        {restaurants.map(recipe => (
+        {outdoorEventsNepal.map(recipe => (
           <Card
             key={recipe.slug}
             title={recipe.title}
@@ -26,13 +26,13 @@ export default function Restaurants() {
 function Card({ title, slug, description, image, imageTitle }) {
   return (
     <div className="rounded-lg shadow-lg bg-white max-w-sm">
-      <Link to={`/restaurants/${slug}`}>
+      <Link to={`/outdoorEventsNepal/${slug}`}>
         <img title={imageTitle} src={image} alt={title}/>
       </Link>
       <div className="p-6">
         <h2 className="text-gray-900 text-lg font-medium mb-2">{title}</h2>
         <p className="text-gray-700">{description}</p>
-        <Link to={`/restaurants/${slug}`} className="inline-block px-6 py-2 bg-blue-600 text-white font-medium text-xs  shadow-md">See Recipe</Link>
+        <Link to={`/outdoorEventsNepal/${slug}`} className="inline-block px-6 py-2 bg-blue-600 text-white font-medium text-xs  shadow-md">See Recipe</Link>
       </div>
     </div>
   );
