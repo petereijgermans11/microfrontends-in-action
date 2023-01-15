d10dacd91df7177c4e67faf9aa9c73feed9cca57
Peter Eijgermans <p.eijgermans11@gmail.com>
2023-01-15 15:49:57 +0100

    added separate mf for outdoorevents

--- micro-frontend-example/layout/src/Header.tsx
+++ micro-frontend-example/layout/src/Header.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex justify-between align-middle p-4 bg-green-700 text-white w-100">
-      <h1>
+    <header className="flex justify-between align-middle p-4 bg-yellow-500 text-white w-100">
+        <h1>
           <a href="/">
-            <h1>Home</h1>
+              <h1><u>Browse outdoor events</u></h1>
           </a>
       </h1>
     </header>
--- micro-frontend-example/outdoorevents/src/OutdoorEvents.tsx
+++ micro-frontend-example/outdoorevents/src/OutdoorEvents.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import {outdoorEvents} from "./data";
 
 export default function OutdoorEvents() {
-  console.log(outdoorEvents);
   return (
     <div className="my-4 px-4 text-center">
-      <h1 className="text-gray-600 text-xl mb-2">Outdoor Events Nepal</h1>
+
+      <h1 className="text-xl mb-1"><strong>Outdoor Events</strong></h1>
+        Search: <input className="text-gray-600" />
+        <br/><br/>
+
       <div className="grid grid-cols-3 gap-4">
         {outdoorEvents.map(event => (
           <Card
@@ -32,7 +35,7 @@ function Card({ title, slug, description, image, imageTitle }) {
       <div className="p-6">
         <h2 className="text-gray-900 text-lg font-medium mb-2">{title}</h2>
         <p className="text-gray-700">{description}</p>
-        <Link to={`/outdoorEvents/${slug}`} className="inline-block px-6 py-2 bg-blue-600 text-white font-medium text-xs  shadow-md">Order event</Link>
+        <Link to={`/outdoorEvents/${slug}`} className="inline-block px-6 py-2 bg-yellow-500 text-white font-medium text-xs  shadow-md">Reserve event</Link>
       </div>
     </div>
   );
